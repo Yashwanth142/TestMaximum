2ec1f3be5a2b58c464e0094d96eda381d11663fd
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-09 15:50:36 +0530

    Find the Max in String added

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@
         public static void Main(String[] args)
         {
 
-            testCases<float> test = new testCases<float>();
+            testCases<string> test = new testCases<string>();
 
-            Console.WriteLine("Given Max Number at 1st Position from {5.5,4.4,3.73} return the Same Number:Test Case 1 ");
-            Console.WriteLine(test.maxi(5.5F, 4.4F, 3.3F));
-            Console.WriteLine("Given Max Number at 2nd Position from {55.8,89.76,73.66} return the Same Number:Test Case 2 ");
-            Console.WriteLine(test.maxi(45.8F, 99.76F, 65.66F));
-            Console.WriteLine("Given Max Number at 3rd Position from {101.56,447.01,833.09} return the Same Number:Test Case 3 ");
-            Console.WriteLine(test.maxi(101.56F,434.01F,703.09F));
+            Console.WriteLine("Given Max Number at 1st Position from {Peach\",\"Apple\", \"Banana}return the Same Number:Test Case 1 ");
+            Console.WriteLine(test.maxi("Peach", "Apple", "Banana"));
+            Console.WriteLine("Given Max Number at 2nd Position from {angle\", \"volume\", \"shape} return the Same Number:Test Case 2 ");
+            Console.WriteLine(test.maxi("angle", "volume", "shape"));
+            Console.WriteLine("Given Max Number at 3rd Position from {lawyer\", \"docter\", \"sofware\"} return the Same Number:Test Case 3 ");
+            Console.WriteLine(test.maxi("lawyer","docter", "sofware"));
         }
     }
 }
