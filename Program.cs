446e27c994c7b440761699b0e4520e85bcc03bad
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-09 15:34:00 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestMaximum
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            testCases<int> test = new testCases<int>();
+
+            Console.WriteLine("Enter three numbers: ");
+
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int c=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The maximum of three number is : "+test.maxi(a,b,c));
+        }
+    }
+}
